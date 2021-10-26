2c7b21a00102f5d636c753a5f1ba8ba19e42e873
fakhar23 <muhammadfakhar111@gmail.com>
2021-10-26 10:45:00 +0500

    Complete 1.0

--- script.js
+++ script.js
@@ -15,12 +15,12 @@ let editTaskOldName = "";
 const clearTasksBtn = document.querySelector(".btn-clear-tasks");
 // let tasksArr = JSON.parse(localStorage.getItem("localTasks"));
 let tasksArr = [
-  ["task1", 0],
-  ["task2", 0],
-  ["task3", 0],
-  ["task4", 0],
-  ["task5", 0],
-  ["task6", 0],
+  ["task 1", 0],
+  ["task 2", 0],
+  ["task 3", 0],
+  ["task 4", 0],
+  ["task 5", 0],
+  ["task 6", 0],
 ];
 
 // loadLocalTasks();
@@ -75,73 +75,98 @@ function renderTasks() {
 }
 
 function clicklistner() {
+  // takes care of deletign task
   btnsCloseTask = document.querySelectorAll(".btn-delete-task");
   btnsCloseTask.forEach((deleteBtn) => {
-    deleteBtn.addEventListener("click", function (e) {
-      const selectedText = e.target
-        .closest(".task-item")
-        .querySelector(".task-text").textContent;
-      // console.log(selectedTask);
-      // const selectedText = selectedTask;
-      console.log(selectedText);
-
-      const index = tasksArr.findIndex((x) => x[0] === selectedText);
-      console.log(index);
-      if (index !== -1) tasksArr.splice(index, 1);
-      renderTasks();
-    });
+    deleteBtn.addEventListener("click", deletignTasks);
   });
 
+  // takes care of makring tasks completed
   document.querySelectorAll(".btn-checkmark").forEach((checkmarkBtn) => {
-    checkmarkBtn.addEventListener("click", function (e) {
-      const selectedTask = e.target.closest(".task-item");
-      console.log(selectedTask);
-      selectedTask.classList.add("completed");
-
-      const selectedText = selectedTask.querySelector(".task-text").textContent;
-      console.log("selectedText: ", selectedText);
-
-      const index = tasksArr.findIndex((x) => x[0] === selectedText);
-      console.log(index);
-      tasksArr[index][1] = 1;
-    });
+    checkmarkBtn.addEventListener("click", completingTasks);
     // renderTasks();
   });
 
+  // takes care of editing tasks
+
   document.querySelectorAll(".btn-outline").forEach((outlineBtn) => {
-    outlineBtn.addEventListener("click", (e) => {
-      const selectedTask = e.target.closest(".task-item");
-      console.log(selectedTask);
+    outlineBtn.addEventListener("click", editingtaks);
+  });
+}
 
-      const selectedText = selectedTask.querySelector(".task-text").textContent;
-      console.log("selectedText: ", selectedText);
+function deletignTasks(e) {
+  const selectedText = e.target
+    .closest(".task-item")
+    .querySelector(".task-text").textContent;
+  // console.log(selectedTask);
+  // const selectedText = selectedTask;
+  console.log(selectedText);
 
-      const index = tasksArr.findIndex((x) => x[0] === selectedText);
-      console.log(index);
-      tasksArr[index][2] = 1;
-      console.log(tasksArr);
-      editTaskOldName = tasksArr[index][0];
-      renderTasks();
-      // now it is displaying edit task input form
+  const index = tasksArr.findIndex((x) => x[0] === selectedText);
+  console.log(index);
+  if (index !== -1) tasksArr.splice(index, 1);
+  renderTasks();
+}
 
-      btnEditTask = document.querySelector(".btn-edit-task");
-      btnEditTask.addEventListener("click", (e) => {
-        btnEditTask = document.querySelector(".btn-edit-task");
-        inputEditTask = document.querySelector(".input-edit-task");
-        formEditTask = document.querySelector(".form-edit-task");
+function completingTasks(e) {
+  const selectedTask = e.target.closest(".task-item");
+  console.log(selectedTask);
+  selectedTask.classList.add("completed");
 
-        formEditTask.addEventListener("submit", (e) => {
-          e.preventDefault();
-          console.log(inputEditTask.value);
+  const selectedText = selectedTask.querySelector(".task-text").textContent;
+  console.log("selectedText: ", selectedText);
 
-          for (let i = 0; i < tasksArr.length; i++)
-            if (tasksArr[i][0] === inputEditTask.value)
-              return alert("task already present");
-          tasksArr[index][0] = inputEditTask.value;
-          tasksArr[index][2] = 0;
-          renderTasks();
-        });
+  const index = tasksArr.findIndex((x) => x[0] === selectedText);
+  console.log(index);
+  tasksArr[index][1] = 1;
+}
+
+function editingtaks(e) {
+  const selectedTask = e.target.closest(".task-item");
+  console.log(selectedTask);
+
+  const selectedText = selectedTask.querySelector(".task-text").textContent;
+  console.log("selectedText: ", selectedText);
+
+  const index = tasksArr.findIndex((x) => x[0] === selectedText);
+  console.log(index);
+  tasksArr[index][2] = 1;
+  console.log(tasksArr);
+  editTaskOldName = tasksArr[index][0];
+  renderTasks();
+  // now it is displaying edit task input form
+  document.querySelector(".input-edit-task").focus();
+
+  // removing event listners from other white we have an edit task form open
+  document.querySelectorAll(".btn-outline").forEach((outlineBtn) => {
+    outlineBtn.removeEventListener("click", editingtaks);
+
+    document.querySelectorAll(".btn-checkmark").forEach((checkmarkBtn) => {
+      checkmarkBtn.removeEventListener("click", completingTasks);
+
+      btnsCloseTask = document.querySelectorAll(".btn-delete-task");
+      btnsCloseTask.forEach((deleteBtn) => {
+        deleteBtn.removeEventListener("click", deletignTasks);
       });
+    });
+  });
+
+  btnEditTask = document.querySelector(".btn-edit-task");
+  btnEditTask.addEventListener("click", (e) => {
+    btnEditTask = document.querySelector(".btn-edit-task");
+    inputEditTask = document.querySelector(".input-edit-task");
+    formEditTask = document.querySelector(".form-edit-task");
+
+    formEditTask.addEventListener("submit", (e) => {
+      e.preventDefault();
+      console.log(inputEditTask.value);
+
+      for (let i = 0; i < tasksArr.length; i++)
+        if (tasksArr[i][0] === inputEditTask.value)
+          return alert("task already present");
+      tasksArr[index][0] = inputEditTask.value;
+      tasksArr[index][2] = 0;
+      renderTasks();
     });
   });
 }
